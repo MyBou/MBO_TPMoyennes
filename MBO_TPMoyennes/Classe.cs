cf09ecfc581a870a31436bf8f716f76690fd71c9
mimosa <mimosa@DESKTOP-8JQKT68>
2021-01-29 14:42:03 +0100

    Ajoutez des fichiers projet.

---
+++ MBO_TPMoyennes/Classe.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MBO_TPMoyennes
+{
+    class Classe
+    {
+        public string nomClasse { get; private set; }
+
+        public List<Eleve> eleves { get; private set; }
+        public List<string> matieres { get; private set; }
+
+        public Classe(string leNomClasse)
+        {
+            nomClasse = leNomClasse;
+            eleves = new List<Eleve>();
+            matieres = new List<string>();
+        }
+
+        public void AjouterEleve(string prenom, string nom)
+        {
+            Eleve eleveClasse = new Eleve(prenom, nom);
+            eleves.Add(eleveClasse);
+        }
+
+        public void AjouterMatiere(string matiere)
+        {
+            matieres.Add(matiere);
+        }
+
+        public double Moyenne(int matiere)
+        {
+            double moyennematiereclasse = 0;
+            for (int i = 0; i < eleves.Count; i++)
+            {
+                double moyennematiereeleve = eleves[i].Moyenne(matiere);
+                moyennematiereclasse += moyennematiereeleve;
+            }
+            moyennematiereclasse = Math.Truncate((moyennematiereclasse / eleves.Count) * 100) / 100;
+            return moyennematiereclasse;
+        }
+
+        public double Moyenne()
+        {
+            double moyennegeneraleclasse = 0;
+            for (int j = 0; j < matieres.Count; j++)
+            {
+                double moyennematieregenerale = Moyenne(j);
+                moyennegeneraleclasse += moyennematieregenerale;
+            }
+            moyennegeneraleclasse = Math.Truncate((moyennegeneraleclasse / matieres.Count) * 100) / 100;
+            return moyennegeneraleclasse;
+        }
+
+    }
+}
---
+++ MBO_TPMoyennes/Eleve.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MBO_TPMoyennes
+{
+    class Eleve
+    {
+        public string nom { get; private set; }
+        public string prenom { get; private set; }
+        public List<Note> notes { get; private set; }
+
+        public Eleve(string nomEleve, string prenomEleve)
+        {
+            nom = nomEleve;
+            prenom = prenomEleve;
+            notes = new List<Note>();
+        }
+
+        public void AjouterNote(Note note)
+        {
+            notes.Add(note);
+        }
+
+        public double Moyenne(int matiere)
+        {
+            double moyennematiere = 0;
+            for (int counter = 0; counter < notes.Count; counter++)
+            {
+                if (notes[counter].matiere == matiere)
+                {
+                    moyennematiere += notes[counter].note;
+                }
+            }
+            moyennematiere = Math.Truncate((moyennematiere / 5) * 100) / 100;
+            return moyennematiere;
+        }
+
+        public double Moyenne()
+        {
+            double moyennegenerale = 0;
+
+            int nbmatieres = 0;
+            double moyennechaquematiere = Moyenne(nbmatieres);
+            do
+            {
+                moyennechaquematiere = Moyenne(nbmatieres);
+                moyennegenerale += moyennechaquematiere;
+                nbmatieres += 1;
+            } while (moyennechaquematiere != 0);
+
+            moyennegenerale = Math.Truncate((moyennegenerale / (nbmatieres - 1)) * 100) / 100;
+            return moyennegenerale;
+        }
+    }
+}
